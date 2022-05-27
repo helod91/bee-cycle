19a1843a8333b0a5c28ab4252fd1d917fca45442
Istvan Szollosi <istvan.szollosi@partner.bmwgroup.com>
2022-05-28 00:18:14 +0300

    Main activity added

--- app/src/main/java/com/accenture/beecycle/di/ViewModelModules.kt
+++ app/src/main/java/com/accenture/beecycle/di/ViewModelModules.kt
@@ -1,11 +1,11 @@
 package com.accenture.beecycle.di
 
-import com.accenture.beecycle.ui.splash.SplashViewModel
+import com.accenture.beecycle.ui.main.MainViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 @ExperimentalCoroutinesApi
 val viewModelModules = module {
-    viewModel { SplashViewModel() }
+    viewModel { MainViewModel() }
 }
